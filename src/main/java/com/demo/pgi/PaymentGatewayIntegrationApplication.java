bc4f938a8c8104698a0f4b05d44c402c9e817135
pks9862728888 <pks9862728888@gmail.com>
2026-09-17 05:29:41 +0530

    Initial commit

---
+++ src/main/java/com/demo/pgi/PaymentGatewayIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/pgi/PaymentGatewayIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
